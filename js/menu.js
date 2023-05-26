dc3f1bba93055f9a3617a42912f4f4bc9466a3d8
Матвей <fantomchik.pubg.m@gmail.com>
2023-05-26 20:45:56 +0300

    1

---
+++ js/menu.js
@@ -0,0 +1,17 @@
+function myFunctionMr() {
+  document.getElementById("myDropdownMr").classList.toggle("show-m-r");
+}
+
+// Закройте выпадающее меню, если пользователь щелкает за его пределами
+/*window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+}; */
---
+++ js/test.js
@@ -0,0 +1,220 @@
+function menu(block) {
+  block.alert("JavaScript");
+}
+
+<header id="header" class="">
+  <div class="container">
+    <div class="in">
+      <div class="site-logo">
+        <a href="/">
+          <noscript>
+            <img
+              src="https://onlinesports.ru/wp-content/uploads/2020/12/logo-onlinesports.svg"
+              alt=""
+            />
+          </noscript>
+          <img
+            class="ls-is-cached lazyloaded"
+            src="https://onlinesports.ru/wp-content/uploads/2020/12/logo-onlinesports.svg"
+            data-src="https://onlinesports.ru/wp-content/uploads/2020/12/logo-onlinesports.svg"
+            alt=""
+          />
+        </a>
+      </div>
+      <div class="mobile-menu-navbar">
+        <ul id="menu-glavnoe-menju-v-shapke" class="menu">
+          <li
+            id="menu-item-321"
+            class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-has-children menu-item-321"
+          >
+            <a href="https://onlinesports.ru/bookmakers/">Рейтинги</a>
+            <ul class="sub-menu">
+              <li
+                id="menu-item-1262"
+                class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1262"
+              >
+                <a href="https://onlinesports.ru/luchshie-bukmekerskie-kontory/">
+                  Рейтинг лучших
+                </a>
+              </li>
+              <li
+                id="menu-item-1263"
+                class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1263"
+              >
+                <a href="https://onlinesports.ru/rejting-nadezhnosti-bukmekerskih-kontor/">
+                  Рейтинг надежности
+                </a>
+              </li>
+              <li
+                id="menu-item-1260"
+                class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1260"
+              >
+                <a href="https://onlinesports.ru/narodnyj-rejting-bukmekerskih-kontor/">
+                  Народный рейтинг
+                </a>
+              </li>
+              <li
+                id="menu-item-1258"
+                class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1258"
+              >
+                <a href="https://onlinesports.ru/rejting-bk-s-bonusom-pri-registracii/">
+                  С приветственным бонусом
+                </a>
+              </li>
+              <li
+                id="menu-item-1259"
+                class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1259"
+              >
+                <a href="https://onlinesports.ru/legalnye-bukmekerskie-kontory/">
+                  Легальные букмекеры
+                </a>
+              </li>
+              <li
+                id="menu-item-1264"
+                class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1264"
+              >
+                <a href="https://onlinesports.ru/rejting-ofshornyh-bukmekerskih-kontor/">
+                  Рейтинг Офшорных
+                </a>
+              </li>
+              <li
+                id="menu-item-1261"
+                class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1261"
+              >
+                <a href="https://onlinesports.ru/zarubezhnye-bukmekerskie-kontory/">
+                  Рейтинг Зарубежных
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li
+            id="menu-item-137"
+            class="menu-item menu-item-type-taxonomy menu-item-object-category current-category-ancestor menu-item-has-children menu-item-137"
+          >
+            <a href="https://onlinesports.ru/forecast-sport/">Прогнозы</a>
+            <ul class="sub-menu">
+              <li
+                id="menu-item-921"
+                class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-921"
+              >
+                <a href="https://onlinesports.ru/forecast-sport/football-forecast-sport/">
+                  Футбол
+                </a>
+              </li>
+              <li
+                id="menu-item-922"
+                class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-922"
+              >
+                <a href="https://onlinesports.ru/forecast-sport/hokkej-forecast-sport/">
+                  Хоккей
+                </a>
+              </li>
+              <li
+                id="menu-item-918"
+                class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-918"
+              >
+                <a href="https://onlinesports.ru/forecast-sport/basketbol-forecast-sport/">
+                  Баскетбол
+                </a>
+              </li>
+              <li
+                id="menu-item-920"
+                class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-920"
+              >
+                <a href="https://onlinesports.ru/forecast-sport/tennis/">
+                  Теннис
+                </a>
+              </li>
+              <li
+                id="menu-item-919"
+                class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-919"
+              >
+                <a href="https://onlinesports.ru/forecast-sport/edinoborstva/">
+                  Единоборства
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li
+            id="menu-item-231"
+            class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-231"
+          >
+            <a href="https://onlinesports.ru/apps/">Приложения</a>
+          </li>
+          <li
+            id="menu-item-136"
+            class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-136"
+          >
+            <a href="https://onlinesports.ru/bonuses/">Бонусы</a>
+          </li>
+          <li
+            id="menu-item-731"
+            class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-731"
+          >
+            <a href="https://onlinesports.ru/promokody/">Промокоды</a>
+          </li>
+          <li
+            id="menu-item-1374"
+            class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-1374"
+          >
+            <a href="#">База знаний</a>
+            <ul class="sub-menu">
+              <li
+                id="menu-item-77"
+                class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-77"
+              >
+                <a href="https://onlinesports.ru/articles/">Стратегии</a>
+              </li>
+              <li
+                id="menu-item-1373"
+                class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-1373"
+              >
+                <a href="https://onlinesports.ru/news/">Новости</a>
+              </li>
+              <li
+                id="menu-item-1372"
+                class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-1372"
+              >
+                <a href="https://onlinesports.ru/oboznacheniya-v-stavkah/">
+                  Обозначения
+                </a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+      <form
+        class="search-form form-style"
+        autocomplete="off"
+        id="searchform"
+        action="https://onlinesports.ru/"
+      >
+        <div class="input-wrapper">
+          <div class="input-wrapper-in">
+            <input
+              type="text"
+              placeholder="Введите поисковой запрос"
+              autocomplete="off"
+              value=""
+              name="s"
+              id="s"
+            />
+            <button type="submit">
+              <svg>
+                <use xlink:href="#icon-loupe-search"></use>
+              </svg>
+            </button>
+          </div>
+          <div class="open-search">
+            <svg>
+              <use xlink:href="#icon-loupe-search"></use>
+            </svg>
+          </div>
+        </div>
+      </form>
+      <div class="open-menu-icon">
+        <span></span>
+      </div>
+    </div>
+  </div>
+</header>;
